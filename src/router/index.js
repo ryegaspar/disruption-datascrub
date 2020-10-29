036318c5c70ec65551247c1b7d7d2d5e048deedd
rye gaspar <gaspar.ryan@outlook.com>
2020-10-28 20:47:41 -0500

    add table configuration page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TableConfiguration from "@/views/TableConfiguration";
 
 Vue.use(VueRouter)
 
@@ -11,14 +12,15 @@ const routes = [
 		component: Home
 	},
 	{
-		path: '/about',
-		name: 'About',
+		path: '/table-configuration',
+		name: 'TableConfiguration',
+		component: TableConfiguration
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
-		component: function () {
-			return import(/* webpackChunkName: "about" */ '../views/About.vue')
-		}
+		// component: function () {
+		// 	return import(/* webpackChunkName: "about" */ '../views/About.vue')
+		// }
 	}
 ]
 
---
+++ src/views/TableConfiguration.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="home">
+        <div class="flex">
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                TIN
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                NPI
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Facility
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Name
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2 whitespace-no-wrap">
+                Add 1
+            </button>
+            <button class="text-gray-800 flex px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2 whitespace-no-wrap">
+                Add 2
+            </button>
+        </div>
+        <div class="flex mt-3">
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                City
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                State
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                ZIP
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Claims
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Claimants
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Procedures
+            </button>
+        </div>
+        <div class="flex mt-3">
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Billed
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                ADA Code
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Network 1
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Network 2
+            </button>
+            <button class="text-gray-800 px-4 py-1 bg-indigo-400 rounded-lg shadow-lg border border-gray-900 hover:bg-indigo-600 mr-2">
+                Network 3
+            </button>
+        </div>
+
+        <hr class="border-b-2 border-gray-900 mt-6 mb-2">
+        <div class="text-gray-600">
+        </div>
+    </div>
+</template>
+<script>
+export default {}
+</script>
