36c793ab5d0e8f7bf98fd8b467fba3e52db85ebe
rye gaspar <gaspar.ryan@outlook.com>
2020-10-28 21:45:29 -0500

    combine sidebar and sidebarmenu into one component

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <!-- Static sidebar for desktop -->
         <div
             class="flex flex-shrink-0 text-white bg-gray-900 overflow-y-scroll -mr-6 min-h-full overflow-hidden h-full">
             <div class="flex flex-col w-48 text-md">
-                <!-- Sidebar component, swap this element with another sidebar if you like -->
                 <div class="flex flex-col h-0 flex-1">
                     <div class="flex-1 flex flex-col">
-                        <side-bar-menus/>
+                        <nav class="flex-1 px-2 py-4 bg-gray-900 space-y-2 font-medium text-sm text-gray-500">
+                            <router-link to="/"
+                                         class="group flex items-center px-2 py-1 leading-normal rounded-md focus:outline-none focus:bg-gray-700 transition ease-in-out duration-300 hover:bg-gray-700 mr-2">
+                                Read Excel
+                            </router-link>
+                            <router-link to="/table-configuration"
+                                         class="group flex items-center px-2 py-1 leading-normal rounded-md focus:outline-none focus:bg-gray-700 transition ease-in-out duration-300 hover:bg-gray-700 mr-2">
+                                Table Configuration
+                            </router-link>
+                        </nav>
                     </div>
                 </div>
             </div>
@@ -16,11 +23,5 @@
 </template>
 
 <script>
-import SideBarMenus from '@/components/SideBarMenus'
-
-export default {
-    components: {
-        SideBarMenus
-    }
-}
+export default {}
 </script>
