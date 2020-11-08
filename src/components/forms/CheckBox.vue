2aae1bc69f74d652138f89e1f7dff194941da58e
rye gaspar <gaspar.ryan@outlook.com>
2020-11-08 11:38:26 -0600

    make icon on the checkbox component optional

--- src/components/forms/CheckBox.vue
+++ src/components/forms/CheckBox.vue
@@ -18,6 +18,7 @@
                     <svg class="h-3 w-3 text-gray-400"
                          fill="none"
                          viewBox="0 0 12 12"
+                         v-show="showIcon"
                     >
                         <path d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
                               stroke="currentColor"
@@ -30,7 +31,11 @@
                     class="opacity-0 ease-out duration-100 absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
                     :class="status ? 'opacity-100 ease-in duration-200' : 'opacity-0 ease-out duration-100'"
                 >
-                    <svg class="h-3 w-3 text-indigo-600" fill="currentColor" viewBox="0 0 12 12">
+                    <svg class="h-3 w-3 text-indigo-600"
+                         fill="currentColor"
+                         viewBox="0 0 12 12"
+                         v-show="showIcon"
+                    >
                         <path
                             d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z"/>
                     </svg>
@@ -53,6 +58,11 @@ export default {
         label: {
             required: true,
             type: String
+        },
+        showIcon: {
+            required: false,
+            type: Boolean,
+            default: false
         }
     },
 }
